207c4ac772a9b4fd432f372c4fd3ea1250c021fe
RASIM\Rasim Dezic <rasim.dezic@my.simpson.edu>
2017-04-11 10:05:48 -0500

    no message

---
+++ rasims_math/__init__.py
@@ -0,0 +1,6 @@
+def sum_two_numbers(a, b):
+    result = a + b
+    return result
+
+my_result = sum_two_numbers(22, 15)
+print(my_result)
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name="rasims_math",
+    packages=["rasims_math"]
+)
